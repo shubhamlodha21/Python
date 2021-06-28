1e91ffec2dc1721c01d8151f0a8dd1a98c5979c2
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-06-28 18:03:38 +0530

    Simple to Multiplication of Two Numbers

---
+++ Multiplication.py
@@ -0,0 +1,11 @@
+#Function Name:Multiplication
+#Input:--
+#Output:200
+#Description:Simple to Multuplication of Two Numbers
+#Date: 28/06/2021
+#Author: Shubham Lodha
+
+a=10
+b=20
+c=a*b
+print("Multiplication is",c)

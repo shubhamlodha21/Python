7d5e42211f30ca68eba7b82829fc356456ec37d8
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-06-30 10:30:15 +0530

    Display Odd Number is in between 10 & 30

---
+++ Range.py
@@ -0,0 +1,9 @@
+#Function Name:Range
+#Input:--
+#Output:11 13 15 17 19 21 23 25 27 29 
+#Description:Display Odd Number is in between 10 & 30
+#Date:30/06/2021
+#Author: Shubham Lodha
+
+r=range(11,31,2)
+for i in r:print(i)

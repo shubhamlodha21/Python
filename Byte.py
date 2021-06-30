277ba4a0a7e73ffc4cdf95bb9e497786d8a85c59
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-06-30 10:26:50 +0530

    Implementation of Bytes

---
+++ Byte.py
@@ -0,0 +1,12 @@
+#Function Name:Byte
+#Input:--
+#Output:10 20 0 40 15
+#Description:Implementation of Bytes
+#Date: 30/06/2021
+#Author: Shubham Lodha
+
+
+element=[10,20,0,40,15]
+x=bytes(element)
+#print (x[3])
+for i in x:  print(i)

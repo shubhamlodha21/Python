a1e9d70777775db8071b76191c871a9062f6a162
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-07-01 14:05:18 +0530

    Return Datatype of each object

---
+++ Datatype.py
@@ -0,0 +1,15 @@
+#Function Name:Datatype
+#Input:--
+#Output:--
+#Description:Return Datatype of each object
+#Date: 01/07/2021
+#Author: Shubham Lodha
+
+
+
+a=15
+print(type(a))
+d={10:"Shubham",11:"Shiva",12:"Hrutik",13:"Bhavna",14:"Prasad"}
+print(type(d))
+element=[10,20,0,40,15]
+print(type(element))

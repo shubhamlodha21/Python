91dd9570195c2619decb733484bf07de426a4db0
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-07-01 14:04:41 +0530

    Addition of Complex Number

---
+++ ComplexAddition.py
@@ -0,0 +1,10 @@
+#Function Name:ComplexAddition
+#Input:--
+#Output:8.5+2j
+#Description:Addition of Complex Number
+#Date: 01/07/2021
+#Author: Shubham Lodha
+
+c1=2.5+2.9j
+c2=6-0.9j
+print("Sum of Complex Number is ",c1+c2)

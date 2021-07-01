f3ee75e72b69c8815e970fb5f89d9fe2b877d811
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-07-01 14:06:00 +0530

    Coverted Number is into Binary,Octal & Hexadecimal

---
+++ NumberSystem.py
@@ -0,0 +1,22 @@
+#Function Name:ComplexAddition
+#Input:11
+
+#Output:
+#Binary of 11 is 0b1011
+#Octal of 11 is 0o13
+#Hexadecimal of 11 is 0xb
+
+#Description:Coverted Number is into Binary,Octal & Hexadecimal
+#Date: 01/07/2021
+#Author: Shubham Lodha
+
+
+a=11
+b=bin(a)
+print("Binary of",a,"is",b)
+
+c=oct(a)
+print("Octal of",a,"is",c)
+
+d=hex(a)
+print("Hexadecimal of",a,"is",d)

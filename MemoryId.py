f036f63b23d6c9b797b2575367bb46c440aab1c3
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-07-05 00:04:49 +0530

    Calculates Memory Location of Object

---
+++ MemoryId.py
@@ -0,0 +1,18 @@
+
+#Function Name:MemoryId
+#Input:--
+
+#Output:
+#2698043874352
+#2698043681520
+
+#Description:Calculates Memory Location of Object 
+#Date: 05/07/2021
+#Author: Shubham Lodha
+
+
+a=151
+b=15
+
+print(id(a))
+print(id(b))

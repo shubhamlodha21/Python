a16c45bf4d680debadcf59cb8729f862faaccf9c
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-07-05 00:07:22 +0530

    Calculates HCF of Numbers

---
+++ hcf.py
@@ -0,0 +1,14 @@
+
+#Function Name:HCF
+#Input:--
+#Output:5
+#Description:Calculates HCF of Numbers
+#Date: 05/07/2021
+#Author: Shubham Lodha
+
+
+import math
+x=15
+y=25
+
+print("HCF is" ,math.gcd(x,y))

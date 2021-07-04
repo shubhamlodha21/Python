901d4b63fea9c5c24887e6ecf46dcd783eec0f0c
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-07-05 00:05:32 +0530

    Use of Logical Operators

---
+++ Logical.py
@@ -0,0 +1,14 @@
+
+#Function Name:Logical
+#Input:--
+#Output:20  10  -10 
+#Description:Use of Logical Operators
+#Date: 05/07/2021
+#Author: Shubham Lodha
+
+
+x=10
+y=20
+print(x and y)
+print(x or y)
+print(not x)

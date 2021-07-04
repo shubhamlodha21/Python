c0185b9a44528576839abd843e9c95d8f9b96bd0
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-07-05 00:06:36 +0530

    Compare Two Objects/Values

---
+++ Equal.py
@@ -0,0 +1,43 @@
+
+#Function Name:Equal
+#Input:--
+#Output:
+#a and b not having same identity
+#a and b doesnt having same value
+
+#x and y not having same identity
+#x and y having same values
+
+#Description:Compare Two Objects/Values
+#Date: 05/07/2021
+#Author: Shubham Lodha
+
+
+a=25
+b=250
+
+if(a is b):
+	print("a and b having same identity")
+else:
+	print("a and b not having same identity")
+
+if(a==b):
+    print("a and b having same values")
+else:
+	print("a and b doesnt having same value")
+
+
+x=[10,20,30,4]
+y=[10,20,30,4]
+
+
+if(x is y):
+	print("x and y having same identity")
+else:
+	print("x and y not having same identity")
+
+if(x==y):
+    print("x and y having same values")
+else:
+	print("x and y doesnt having same value")
+

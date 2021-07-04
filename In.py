fa2fc9855f5609287d1d68b39264d1ef571cf881
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-07-05 00:07:48 +0530

    Use of in Keyword

---
+++ In.py
@@ -0,0 +1,14 @@
+
+#Function Name:In
+#Input:--
+#Output:[10,20,50,60,90]
+#Description:Use of in Keyword
+#Date: 05/07/2021
+#Author: Shubham Lodha
+
+
+
+numbers=[10,20,50,60,90]
+
+for i in numbers:
+	print(i)

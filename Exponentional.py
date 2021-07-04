ddcd739f1e6f214ad656653cb027db6ca8edf315
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-07-04 09:39:07 +0530

    Power of numbers

---
+++ Exponentional.py
@@ -0,0 +1,13 @@
+
+#Function Name:Exponentional
+#Input:--
+#Output:Exponemtial/Power is 1000
+#Description:Power of Number 
+#Date: 04/07/2021
+#Author: Shubham Lodha
+
+
+
+a=10
+b=3
+print("Exponential/Power  is",a**b)

699010da8e212ffd260cb04fb2282249fa996f75
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-07-06 08:22:22 +0530

    Take two Numbers from user and return their Division

---
+++ Division.py
@@ -0,0 +1,13 @@
+#Function Name:Division
+#Input:20  2
+#Output:Division of 20 and 2 is 10
+#Description:Take two Numbers from user and return their Division
+#Date: 06/07/2021
+#Author: Shubham Lodha
+
+no1=float(input("Enter a First Number"))
+no2=int(input("Enter a Second Number"))
+
+print('Subustraction of',no1,'And',no2,'is',no1/no2)
+
+print('Subustraction of {} and {} is {}'.format(no1,no2,no1/no2))

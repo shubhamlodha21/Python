d11466353b323d6242e3fa219153706f82b0361a
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-07-06 08:20:14 +0530

    Take two Numbers from user and return their Subtraction

---
+++ Substraction.py
@@ -0,0 +1,13 @@
+#Function Name:Substraction
+#Input:20  18
+#Output:Subustraction of 20 and 18 is 2
+#Description:Take two Numbers from user and return their Substraction
+#Date: 06/07/2021
+#Author: Shubham Lodha
+
+no1=float(input("Enter a First Number"))
+no2=int(input("Enter a Second Number"))
+
+print('Subustraction of',no1,'And',no2,'is',no1-no2)
+
+print('Subustraction of {} and {} is {}'.format(no1,no2,no1-no2))

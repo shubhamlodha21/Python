793bd04eaaaea04ce9afa0424311129a73a28c6b
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-07-06 08:19:29 +0530

    Take an input from user and return it.

---
+++ Input.py
@@ -0,0 +1,10 @@
+#Function Name:Input
+#Input:20 
+#Output:Entered Number is 20
+#Description:Take an input from user and return it.
+#Date: 06/07/2021
+#Author: Shubham Lodha
+
+str=input("Enter a Number")
+
+print("Entered Number is",str)

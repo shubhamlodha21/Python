098d30b72c5ddb1829ec00a0f1fd4e34b2029f01
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-07-06 08:18:13 +0530

    Enter any three number and return their addition which are seperated by @

---
+++ Addition3.py
@@ -0,0 +1,10 @@
+#Function Name:Addition3
+#Input:20 30 10 
+#Output:60
+#Description:Enter any three number and return their addition which are seperated by @ 
+#Date: 06/07/2021
+#Author: Shubham Lodha
+
+var1,var2,var3=[int(x) for x in input("Enter Numbers").split('@')]
+
+print("Summation is ",var1+var2+var3)

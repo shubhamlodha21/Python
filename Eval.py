eab537aa1d11d9f3de70e60b47eef7e8c660bba8
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-07-06 08:18:54 +0530

    Enter any expression and get solution of it

---
+++ Eval.py
@@ -0,0 +1,9 @@
+#Function Name:Eval
+#Input:10*2-2+8-6
+#Output:20
+#Description:Enter any expression and get solution of it
+#Date: 06/07/2021
+#Author: Shubham Lodha
+
+x=eval(input("Enter an Expression:"))
+print("Solution of Expression is",x)

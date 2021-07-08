b9c5822865972af53c920079952179ac378e72b8
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-07-08 07:06:37 +0530

    Print the Pattern

---
+++ Pattern.py
@@ -0,0 +1,24 @@
+#Function Name:Pattern
+#Input:--
+#Output:
+
+# *
+# * *
+# * * *
+# * * * *
+# * * * * *
+# * * * * * *
+# * * * * * * *
+# * * * * * * * *
+# * * * * * * * * *
+# * * * * * * * * * *
+
+
+#Description:Print the Pattern
+#Date: 08/07/2021
+#Author: Shubham Lodha
+
+for i in range(1,11):
+	for j in range(1,i+1):
+		print("*",end=" ")
+	print()

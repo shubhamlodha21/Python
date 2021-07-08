61436470e58967e23eb184c8efc48e60beffe881
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-07-08 07:03:54 +0530

    Used of assert

---
+++ Assert.py
@@ -0,0 +1,10 @@
+#Function Name:Assert
+#Input:12
+#Output:Error:"Number Zero Allowed"
+#Description:Used of assert  
+#Date: 08/07/2021
+#Author: Shubham Lodha
+
+n=int(input("Entered a Number"))
+assert n==0,"Number Zero Allowed"
+print("You Entered",n)

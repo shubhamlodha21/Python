75bf47234f90ad732ec82cf3eca9efa97d52779d
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-07-08 07:05:43 +0530

    Use of Pass Keyword

---
+++ Pass.py
@@ -0,0 +1,15 @@
+#Function Name:Pass
+#Input:
+#Output:0 1 2 3 4 5 6 7 8 9 
+#Description:Used of Pass 
+#Date: 08/07/2021
+#Author: Shubham Lodha
+
+x=10
+while x>=1:
+  
+   x=x-1
+   if x>5:
+   	pass
+   print(x)
+print("End of Loop")

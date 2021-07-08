1afb64accad83242c582656a3024446039c579e0
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-07-08 07:05:10 +0530

    Use of character Array

---
+++ CharArray.py
@@ -0,0 +1,16 @@
+#Function Name:CharArray
+#Input:--
+#Output:Elements of Array is
+        #a
+        #b
+        #c
+        #d
+#Description:Display Elements of Character type Array
+#Date: 08/07/2021
+#Author: Shubham Lodha
+
+from array import *
+arr=array('u',['a','b','c','d'])
+print("Elements of Array is")
+for ch in arr:
+	print(ch)

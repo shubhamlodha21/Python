52ecf70b1ea6aaaf891981e083bcc388c2bc4b05
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-07-09 09:21:19 +0530

    Nested for-loop Nested Statements

---
+++ Nested.py
@@ -0,0 +1,24 @@
+#Function Name:Nested
+#Input:
+#Output:
+
+#i= 0     j= 0
+#i= 0     j= 1
+#i= 0     j= 2
+#i= 0     j= 3
+#i= 1     j= 0
+#i= 1     j= 1
+#i= 1     j= 2
+#i= 1     j= 3
+#i= 2     j= 0
+#i= 2     j= 1
+#i= 2     j= 2
+#i= 2     j= 3
+
+#Description:Nested for-loop Nested Statements
+#Date: 09/07/2021
+#Author: Shubham Lodha
+
+for i in range(3):
+	for j in range(4):
+		print("i=",i,"\t","j=",j)

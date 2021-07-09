9ea138c80b6c1edc3be2f4b3d1f76b6cff239c72
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-07-09 09:22:34 +0530

    Use of continue statement

---
+++ Continue.py
@@ -0,0 +1,15 @@
+#Function Name:Continue
+#Input:
+#Output:5 4 3 2 1 0
+#Description:Use of continue statement
+#Date: 09/07/2021
+#Author: Shubham Lodha
+
+x=10
+while x>=1:
+  
+   x=x-1
+   if x>5:
+   	continue
+   print(x)
+print("End of Loop")

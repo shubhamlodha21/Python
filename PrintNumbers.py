f8c588c7cd2613d719b8604844c1ce99127f1aa4
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-07-09 09:19:08 +0530

    Display Numbers from 1 to 20

---
+++ PrintNumbers.py
@@ -0,0 +1,13 @@
+#Function Name:PrintNumbers
+#Input:20
+#Output:1 2 3 4.......19 20 
+#Description:Display Numbers from 1 to 20
+#Date: 09/07/2021
+#Author: Shubham Lodha
+
+n=int(input("Enterd a Number"))
+x=1
+while x<=n:
+	print(x)
+	x=x+1
+print("End")

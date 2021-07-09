629993595663dab8850422e7803145c146fef9a5
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-07-09 09:20:43 +0530

    Use of if-else statement

---
+++ Between.py
@@ -0,0 +1,13 @@
+#Function Name:Between
+#Input:10
+#Output:10 is grater than 9
+#Description:Use of if-else statement
+#Date: 09/07/2021
+#Author: Shubham Lodha
+
+n=(int(input("Enter a Number")))
+
+if n>0 and n<10:
+	print(n, "is Between 0 to 9")
+else:
+	print(n,"is greater than 9")

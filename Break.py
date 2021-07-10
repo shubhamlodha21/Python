2cc09726822dc037ee6c9442633f9b87ac12c8cf
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-07-10 07:51:33 +0530

    Use of break keyword

---
+++ Break.py
@@ -0,0 +1,14 @@
+#Function Name:Break
+#Input:--
+#Output:10 9 8 7 6 
+#Description:Use of break statement
+#Date: 10/07/2021
+#Author: Shubham Lodha
+
+x=10
+while x>=1:
+   print(x)
+   x=x-1
+   if x==5:
+   	break;
+print("End of Loop")

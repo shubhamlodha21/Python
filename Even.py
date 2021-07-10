c5d6a897dd73e89a4a1ae6b26b58bd1d72618bc4
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-07-10 07:53:02 +0530

    Display Even Numbers is in between 100 to 200

---
+++ Even.py
@@ -0,0 +1,13 @@
+#Function Name:Even
+#Input:--
+#Output:100 102 104....196 198 200 
+#Description:Display Even Numbers is in between 100 to 200
+#Date: 10/07/2021
+#Author: Shubham Lodha
+
+
+no=100
+while no<=200:
+	  print(no)
+	  no=no+2
+	 

b67a238c77d16e466684cf66d0d799be21227c73
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-07-12 00:55:10 +0530

    Enter String and Count number of characters

---
+++ CountChar.py
@@ -0,0 +1,13 @@
+#Function Name:CountChar
+#Input:Shubhamamaa  (a)
+#Output:4
+#Description:Enter String and Count number of characters
+#Date: 12/07/2021
+#Author: Shubham Lodha
+
+
+str=input("Enter String")
+ch=input("Enter Character Whose Count You Want:")
+
+count=str.count(ch)
+print("Count of Character",ch,"is:",count)

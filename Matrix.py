2848dabfdcdf3ccf2e665d6ccb417ad44db0c0f5
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-07-11 09:06:53 +0530

    Creates 2-D matrix

---
+++ Matrix.py
@@ -0,0 +1,13 @@
+#Function Name:Matrix
+#Input: 
+#Output:2-D Matrix is [1,2,3]
+#                     [4,5,6]
+#Description:Creates 2-D matrix 
+#Date: 11/07/2021
+#Author: Shubham Lodha
+
+
+from numpy import *
+arr=[[1,2,3],[4,5,6]]
+a=matrix(arr)
+print("2-D Matrix is:",a)

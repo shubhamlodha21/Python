d4c56463538f0aab40b81f21347d9499b1c524dd
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-07-14 00:52:43 +0530

    Accept Word from user and reverse it.

---
+++ ReverseWord.py
@@ -0,0 +1,15 @@
+#Function Name:ReverseWord
+#Input:Shubham
+#Output:mahbuhS
+#Description:Accept Word from user and reverse it.
+#Date: 14/07/2021
+#Author: Shubham Lodha
+
+
+
+print("Enter String:")
+s=str(input())
+
+i=-1
+for i in s[ : : -1]:
+	print(i,end="")

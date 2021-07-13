515d7b4a2d7a745bc1c0a5fa1f555829b74968c7
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-07-13 18:01:43 +0530

    Use of if keyword

---
+++ If.py
@@ -0,0 +1,19 @@
+#Function Name:If
+#Input:
+
+#Output:False
+#       Value is Two
+#       End
+
+#Description:Use of if keyword
+#Date: 13/07/2021
+#Author: Shubham Lodha
+
+num=2
+if num==1:
+	print("True")
+	print("Value is One")
+	if num==2:
+		print("False")
+		print("Value is Two")
+print("End")

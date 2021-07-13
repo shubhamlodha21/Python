cf826cd9e7c4d5026ce7844516bfe646aef5c467
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-07-13 18:03:07 +0530

    Display any n random numbers

---
+++ Random.py
@@ -0,0 +1,10 @@
+#Function Name:Multiplications
+#Input:
+#Output:Random Numbers are: [0.43475639 0.51960347 0.25598442 0.31182447 0.2009212 ]
+#Description:Display any 5 random numbers
+#Date: 13/07/2021
+#Author: Shubham Lodha
+
+from numpy import *
+a=random.rand(5)
+print("Random Numbers are:",a)

95072c9b2f9c3702c94009355a34f0fddd7f2b09
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-07-14 00:51:03 +0530

    Creates Array by using Numpy

---
+++ Numpy.py
@@ -0,0 +1,12 @@
+#Function Name:Numpy
+#Input:
+#Output:Elements are [10,20.9,30.8,40,50]
+#Description:Creates Array by using Numpy
+#Date: 14/07/2021
+#Author: Shubham Lodha
+
+
+from numpy import *
+
+arr=array([10,20.9,30.8,40,50])
+print("Elements are:",arr)

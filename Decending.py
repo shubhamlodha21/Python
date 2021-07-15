c4fc9ad7b5f1287ac5a1b0c5d383af37d97e7a33
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-07-16 01:22:35 +0530

    Display Numbers in decending order

---
+++ Decending.py
@@ -0,0 +1,9 @@
+#Function Name:Decending
+#Input: 
+#Output:10 9 8 7 6 5 4 3 2 1
+#Description:Dispay Numbers in Decending order
+#Date: 16/07/2021
+#Author:Shubham Lodha
+
+for x in range(10,0,-1):
+	print(x,end=" ")

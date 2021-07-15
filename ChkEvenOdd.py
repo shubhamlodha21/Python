55c1eab7c79ab9f6ebe78c90f12acedc87fecef5
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-07-16 01:24:00 +0530

    Check Whether given number is Odd or Even

---
+++ ChkEvenOdd.py
@@ -0,0 +1,18 @@
+#Function Name:ChkEvenOdd
+#Input: 
+#Output:15 is Odd Number
+#       100 is Even Number
+#Description:Check Whether given number is Odd or Even
+#Date: 16/07/2021
+#Author:Shubham Lodha
+
+
+
+def Even_Odd(num):
+	if num % 2==0:
+		print(num,"is Even Number")
+	else:
+		print(num,"is Odd Number")
+
+Even_Odd(15)
+Even_Odd(100)

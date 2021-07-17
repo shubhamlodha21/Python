f63c32dceec37c39f23327b35dd2c22712c68ea1
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-07-17 08:07:22 +0530

    Calls Function and return addition and Multiplications of Numbers

---
+++ Function.py
@@ -0,0 +1,21 @@
+#Function Name:Function
+#Input:--
+
+#Output:Sum=  25
+#Multiplication=  150
+#Sum=  9.6
+#Multiplication=  10.08
+
+#Description:Calls Function and return addition and Multiplications of Numbers
+#Date: 17/07/2021
+#Author: Shubham Lodha
+
+def Sum(a,b):
+	c=a+b
+	d=a*b
+
+	print("Sum= ",c)
+	print("Multiplication= ",d)
+
+Sum(10,15)
+Sum(1.2,8.4)

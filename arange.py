7997d37f35667d4c2af7410c258dab010ce80b91
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-07-17 08:08:21 +0530

    Arrange the elements in between the range

---
+++ arange.py
@@ -0,0 +1,25 @@
+#Function Name:arange
+#Input
+
+#Output:
+#[0 1 2 3 4 5 6 7 8 9]
+#[5 6 7 8 9]
+#[11 13 15 17 19 21]
+
+#Description:Arrange the elements in between the range
+#Date: 17/07/2021
+#Author: Shubham Lodha
+
+
+
+
+from numpy import *
+
+a=arange(10)
+print(a)
+
+a=arange(5,10)
+print(a)
+
+a=arange(11,22,2)
+print(a)

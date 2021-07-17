f02c8e4efa7982cfbbb8c4d5cbd03367a76775eb
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-07-17 08:07:49 +0530

    Creates the Array of zero and ones

---
+++ zerosones.py
@@ -0,0 +1,19 @@
+#Function Name:zerosones
+#Input:--
+
+#Output:
+#Zeros Array is [0 0 0 0 0]
+#Ones Array is  [1. 1. 1. 1. 1.]
+
+#Description:Creates the Array of zero and ones
+#Date: 17/07/2021
+#Author: Shubham Lodha
+
+
+
+from numpy import *
+a=zeros(5,int)
+b=ones(5,float)
+
+print("Zeros Array is",a)
+print("Ones Array is ",b)

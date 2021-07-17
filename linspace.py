b0755f27234e72524da7cfd8babfe083bf86e4df
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-07-17 08:08:52 +0530

    Display Elements using linspace

---
+++ linspace.py
@@ -0,0 +1,13 @@
+#Function Name:linspace
+#Input
+
+#Output:
+#Elements Are:  [10.  12.5 15.  17.5 20]
+
+#Description:Display Elements using linspace
+#Date: 17/07/2021
+#Author: Shubham Lodha
+
+from numpy import *
+a = linspace(10,20,5)
+print("Elements Are: ",a)

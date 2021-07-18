5f31da5b2a04f1e6a77f26e58a6c4308124a2e35
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-07-18 10:16:08 +0530

    Sum of n numbers of array

---
+++ Sumn.py
@@ -0,0 +1,15 @@
+#Function Name:Sumn
+#Input: 
+#Output:150       
+#Description:Sum of n numbers of array
+#Date: 18/07/2021
+#Author: Shubham Lodha
+
+
+
+list=[10,20,30,40,50]
+sum=0
+for x in list:
+	sum=sum+x
+	print(x)
+print("Sum of Elements is",sum)

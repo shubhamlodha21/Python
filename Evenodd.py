16a987cb8fb0637b5b658e03c035a5629bb274ee
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-07-18 10:16:48 +0530

    Enter Number from user and display it is Even or Odd

---
+++ Evenodd.py
@@ -0,0 +1,15 @@
+#Function Name:Evenodd
+#Input: 25
+#Output:25 is Odd Number        
+#Description:Enter Number from user and display it is Even or Odd
+#Date: 18/07/2021
+#Author: Shubham Lodha
+
+
+
+n=int(input("Enter a Number"))
+
+if n%2==0:
+	print(n,"is Even Number")
+else:
+	print(n,"is Odd Number")

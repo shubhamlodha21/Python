5bbaeffdb143ef780e6d178cc6de5853788f6506
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-07-18 10:15:14 +0530

    Display Hello Python in Hindi

---
+++ Hindi.py
@@ -0,0 +1,10 @@
+#Function Name:Hindi
+#Input: 
+#Output:हॅलो  पायथॉन        
+#Description:Display Hello Python in Hindi
+#Date: 18/07/2021
+#Author: Shubham Lodha
+
+
+name=u'\u0915\u094b\u0930  \u092a\u0948\u0925\u0964\u0928 '
+print(name)

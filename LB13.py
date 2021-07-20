2ec1e3a0516c258e3bb228bc5861a8f14df21716
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-07-21 01:09:07 +0530

    Program to print 5 to 1 numbers on screen.

---
+++ LB13.py
@@ -0,0 +1,11 @@
+#Function Name:LB13
+#Input:--
+#Output:5  4  3  2  1
+#Description:Program to print 5 to 1 numbers on screen.
+#Date: 21/07/2021
+#Author: Shubham Lodha
+
+
+
+for i in range (5,0,-1):
+	print(i)

fc4f3d1a504215a1a7304e441c6ac8885ac8c924
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-07-21 01:07:31 +0530

    Accept Two Numbers from user and return division

---
+++ LB11.py
@@ -0,0 +1,14 @@
+#Function Name:LB11
+#Input:15 5
+#Output:3
+#Description:Accept Two Numbers from useer and return division
+#Date: 21/07/2021
+#Author: Shubham Lodha
+
+
+iNo1=int(input("Enter First Number"))
+iNo2=int(input("Enter Second number"))
+
+iNo3=iNo1/iNo2
+
+print("Division is:",float(iNo3))

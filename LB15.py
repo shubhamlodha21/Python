9af2e1b661a0391f42c79007e61eb0ee8490ed49
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-07-21 01:09:53 +0530

    Accept one number from user and print that number of * on screen.

---
+++ LB15.py
@@ -0,0 +1,11 @@
+#Function Name:LB15
+#Input:3
+#Output:*	*	*
+#Description:Accept one number from user and print that number of * on screen. 
+#Date: 21/07/2021
+#Author: Shubham Lodha
+
+
+iNo=int(input("Enter Number"))
+for i in range (iNo):
+	print("*\t",end=" ")

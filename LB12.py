c0daf239d1234d1db715692671ad42152ead5f33
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-07-21 01:08:31 +0530

    Program to print 5 times “Marvellous” on screen

---
+++ LB12.py
@@ -0,0 +1,9 @@
+#Function Name:LB12
+#Input:--
+#Output:--
+#Description:Program to print 5 times “Marvellous” on screen.
+#Date: 21/07/2021
+#Author: Shubham Lodha
+
+for i in range (5):
+	print("Marvellous")

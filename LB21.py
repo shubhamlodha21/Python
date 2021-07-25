102fcded29325edd185bcd96328cc63ce9eb1abe
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-07-26 00:03:47 +0530

    Accept number from user and check whether number is even or odd

---
+++ LB21.py
@@ -0,0 +1,10 @@
+#Function Name:LB21
+#Input:3
+#Output:* * *
+#Description:Accept one number from user and print that number of * on screen(For Loop)
+#Date: 26/07/2021
+#Author: Shubham Lodha
+
+no=int(input("Enter Number"))
+for i in range(1,no+1,1):
+	print("*",end=" ")

8b0a96d14a2424ba4a6814f28616a335ff519db2
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-07-26 00:04:42 +0530

    Accept on number from user if number is less than 10 then print “Hello” otherwise print “Demo”.

---
+++ LB23.py
@@ -0,0 +1,12 @@
+#Function Name:LB23
+#Input:31
+#Output:Demo
+#Description:Accept on number from user if number is less than 10 then print “Hello” otherwise print “Demo”.
+#Date: 26/07/2021
+#Author: Shubham Lodha
+
+No=int(input("Enter a Number"))
+if(No<10):
+	print("Hello")
+else:
+	print("Demo")

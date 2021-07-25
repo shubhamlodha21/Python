746b5ba5916593b0e39a9c0e2dc8e2a0668b91f6
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-07-26 00:04:13 +0530

    Accept one number from user and print that number of * on screen(while Loop)

---
+++ LB22.py
@@ -0,0 +1,12 @@
+#Function Name:LB22
+#Input:3
+#Output:* * *
+#Description:Accept one number from user and print that number of * on screen(while Loop)
+#Date: 26/07/2021
+#Author: Shubham Lodha
+
+
+no=int(input("Enter Number"))
+while(no>0):
+	print("*",end=" ")
+	no=no-1

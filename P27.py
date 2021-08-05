2cd40706f10cc80f1ada893c9458178f78d088a1
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-08-05 13:41:52 +0530

    Print the Pattern

---
+++ P27.py
@@ -0,0 +1,28 @@
+#Function Name:Pattern
+#Input:5
+#Output:
+
+# 1 2 3 4 5 
+# 1 2 3 4 5
+# 1 2 3 4 5
+# 1 2 3 4 5 
+# 1 2 3 4 5
+
+#Description:Print the pattern
+#Date: 05/08/2021
+#Author: Shubham Lodha
+
+
+
+def Pattern(no):
+    for i in range(no):
+        for j in range(1,no+1,1):
+            print(j,end=" ")
+        print("")
+
+def main():
+    no1=int(input("Enter Number"))
+    Pattern(no1)
+
+if __name__=="__main__":
+    main()

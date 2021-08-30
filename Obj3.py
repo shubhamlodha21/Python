c4d67e50293cad1307d9ef565a54818107dd3d17
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-08-30 23:35:50 +0530

    Accepting N num and making a list.

---
+++ Obj3.py
@@ -0,0 +1,13 @@
+#Discription: Accepting N num and making a list.
+#Date: 30/08/21
+#Author : Shubham lodha
+
+class Arithematic:
+	def __init__(self):                                                                                                                                                                    
+		print("Inside Constructor")
+
+def main():
+	obj=Arithematic()
+	
+if __name__ == '__main__':
+		main()	

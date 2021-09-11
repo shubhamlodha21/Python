b5f6b9b32848dc8ae9e6ae04803151a0cfc0b601
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-09-11 18:44:48 +0530

    Star pattern printing

---
+++ pattern2.py
@@ -0,0 +1,19 @@
+'''input:--
+output:- 
+        * 
+       ** 
+      ***
+     ****
+    *****
+description:left half Star Pyramid 
+date: 28-08-2021
+Author name: Shruti Nahar'''
+x=5
+m=(2*x)-2
+for i in range(0,x):
+    for j in range(0,m):
+        print(end=" ")
+    m=m-1
+    for j in range(0,i+1):
+        print("*",end='')
+    print(" ")

ca885a64a7918e168448746016fe2accdda72cc7
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-09-15 19:19:02 +0530

    Implementation of Arithmetic Operator

---
+++ Arithmeticoperator.py
@@ -0,0 +1,21 @@
+
+'''input:--
+output:Num1 + Num2=  30
+       Num1 - Num2=  -10
+       Num1 * Num2=  200
+       Num1 / Num2=  0.5
+       5^3=  125
+       20%3=  2
+       22//7=  3
+description:perform Arithmetic Operation
+date: 24-08-2021
+Author name: Shruti Nahar'''
+num1=10
+num2=20
+print("Num1 + Num2= ",num1+num2)
+print("Num1 - Num2= ",num1-num2)
+print("Num1 * Num2= ",num1*num2)
+print("Num1 / Num2= ",num1/num2)
+print("5^3= ",5**3);
+print("20%3= ",20%3)
+print("22//7= ",22//7)

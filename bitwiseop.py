dee00dcf14d78f79c73c0fe88f3e11d16a04f8ab
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-09-15 19:21:34 +0530

    Implementation of bitwise Operator

---
+++ bitwiseop.py
@@ -0,0 +1,17 @@
+'''input:- --
+output:-
+num1 | num2:  7
+num1 & num2:  5
+num1 ^ num2:  2
+num1 right shift num2:  0 
+num1 left shift num2:  224
+description: Bitwise operator 
+date: 27-08-2021
+Author name: Shruti Nahar'''
+num1=7
+num2=5
+print("num1 | num2: ",num1|num2)
+print("num1 & num2: ",num1&num2)
+print("num1 ^ num2: ",num1^num2)
+print("num1 right shift num2: ",num1>>num2)
+print("num1 left shift num2: ",num1<<num2)

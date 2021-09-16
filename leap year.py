d445789d6a443bfee1286439c2a00bb5cc4446e6
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-09-16 11:35:35 +0530

    Check whether the year is leap or not

--- leap year.py
+++ leap year.py
@@ -1,3 +1,8 @@
+'''input:Enter the year 2020
+output:Leap Year
+Description:-Check whether the year is leap or not
+date: 16-09-2021
+Author name: Shubham Lodha'''
 year=int (input("Enter the year "))
 if(year%400==0) or (year%4==0):
     print("Leap Year")

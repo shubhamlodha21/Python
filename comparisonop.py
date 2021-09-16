0d11a02e8cf4cf412b7f4d00f93f98258c360119
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-09-16 11:31:43 +0530

    Implementation of Comparison Operator

--- comparisonop.py
+++ comparisonop.py
@@ -3,8 +3,8 @@
 #        Is num1 = num2  False
 #        Is num1 != num2  True
 #description:-Implementation of Comparison Operator
-#date: 25-08-2021
-#Author name: Shruti Nahar
+#date: 16-09-2021
+#Author name: Shubham Lodha
 num1=20
 num2=10 
 print("Is num1 > num2 ? ",num1>num2)

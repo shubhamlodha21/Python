9546caa3cbcee97bb9144430ff02aec673f62dc0
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2022-01-22 23:06:59 +0530

    Display Fibonacci Series upto n numbers

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,17 +1,11 @@
-#Function Name:Fibonacci
-#Input:7
-#Output:1 1 2 3 5 8  
-#Description:Display Fibonacci Series upto n numbers
-#Date: 09/07/2021
-#Author: Shubham Lodha
+#Display Fibonacci Series upto n numbers
 
-
-n=int(input("upto which u want Number"))
-a=b=1
-print(a)
-print(b)
-for i in range(1,n-1):
-	c=a+b
-	print(c)
-	a=b
-	b=c
+n=int(input("How Many Numbers you want in Series:"))
+f1=0;f2=1;f3=0
+print("0",end=" ")
+for i in range (1,n):
+    f3=f1+f2
+    print(f3,end=" ")
+    f1=f2
+    f2=f3
+    f3=f1

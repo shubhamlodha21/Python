56bf18a9434e82bf001f2f75869256f8407a3626
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2022-03-06 11:06:36 +0530

    Check Substring is Present or not not in main string

---
+++ Substring.py
@@ -0,0 +1,9 @@
+#Check Substring is Present or not not in main string 
+str1=str(input("Enter the main String:"))
+str2=str(input("Enter the substring:"))
+
+if str2.find(str1):
+    print("Substring is Find !!")
+else:
+    print("Substring is not present!")
+   

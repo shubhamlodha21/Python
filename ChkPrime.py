f139cade2be28dab611c2d9d91a8ff739093bf37
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2022-03-06 11:04:47 +0530

    Check whether given number is prime or not

--- ChkPrime.py
+++ ChkPrime.py
@@ -1,24 +1,13 @@
-#Function Name:Prime
-#Input: 17
-#Output:17 is Prime Number
-#Description:Accept Number from user and check number is Prime
-#Date: 16/07/2021
-#Author:Shubham Lodha
+# Check whether given number is prime or not
+def ChkPrime(n):
+    for i in range (2,n):
+        if(n%i)==0:
+            return 1
+    return 0
 
-
-def Prime(n):
-	x=1
-	for i in range(2,n):
-		if n%i==0:
-			x=0
-			break
-		else:
-			x=1
-	return x
-
-num=int(input("Enter a Number"))
-result=Prime(num)
-if result==1:
-	print(num,"is Prime Number")
+iNo=int(input("Enter Number to check prime or Not ?"))
+x=ChkPrime(iNo)
+if(x==1):
+    print("Not Prime Number")
 else:
-	print(num,"is Not Prime Number")
+    print(" Prime Number")

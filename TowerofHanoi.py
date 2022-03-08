d493751d34ae21920edcbaa4eeecb7cb5e437a64
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2022-03-08 19:26:49 +0530

    Tower of Hanoi

---
+++ TowerofHanoi.py
@@ -0,0 +1,10 @@
+def Towers(n,a,c,b):
+    if(n==1):
+        print("Move Disk %i from pole %s to pole %s" %(n,a,c))
+    else:
+        Towers(n-1,a,b,c)
+        print("Move disk %i from pole %s to pole %s" %(n,a,c))
+        Towers(n-1,b,c,a)
+
+n=int(input("Enter number of Disks:"))
+Towers(n,"A","C","B")
